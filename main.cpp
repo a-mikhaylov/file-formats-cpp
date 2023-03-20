7629f2957b3f9694391ed430ce7f99f462f813a8
a-mikhaylov <lexarobotr2d2@gmail.com>
2023-03-20 15:14:56 +0300

    add library for working

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
+#include <h5cpp/hdf5.hpp>
 #include "libs/hdf5-test.h"
 
 int main() {
-    std::cerr << "nothing here" << std::endl;
+    auto type = hdf5::datatype::TypeTrait<int>::create();
+    std::cout<<type.get_class()<<std::endl;
 }
